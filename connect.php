8160e957bc3492825925447424845fc8b1a458e0
Zani <serxhio.zani@ifm.com>
2024-04-22 09:57:40 +0200

    update 22.04.24

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$username = $_POST['username'];
+$email = $_POST['email'];
+$name = $_POST['name'];
+$vorname = $_POST['vorname'];
+$adresse = $_POST['adresse'];
+$plz = $_POST['plz'];
+$stadt = $_POST['stadt'];
+$unternehmen = $_POST['unternehmen'];
+$beruf = $_POST['beruf'];
+$password = $_POST['password'];
+
+
+?>
